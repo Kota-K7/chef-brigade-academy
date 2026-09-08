cdbc01f87ed296d5b43b44b88c4d35fa72d3b2d7
Kota-K7 <hangtaishengyi@gmail.com>
2026-09-08 20:31:53 +0800

    aistudio

--- scratch/inspect_startepisode.py
+++
@@ -1,9 +0,0 @@
-with open('js/views/story.js', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines):
-    if 'async function startEpisode' in line:
-        start = i
-        break
-
-print(''.join(lines[start:start+70]))
